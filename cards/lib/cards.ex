6338596004f59bac834ae8c9c9f05c090307e8af
Marlon Peseke <mpeseke@gmail.com>
2023-05-03 13:46:47 -0600

    testing

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,46 +1,75 @@
 defmodule Cards do
-  @moduledoc """
-  Documentation for `Cards`.
-  """
+@moduledoc """
+  Provides methods for creating and handling a deck of cards.
+"""
 
-  def create_deck do
-    values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
-    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+@doc """
+  Returns a list of strings representing a deck of playing cards.
+"""
+def create_deck do
+  values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
+  suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
 
-    for suit <- suits, value <- values do
-      "#{value} of #{suit}"
-    end
+  for suit <- suits, value <- values do
+    "#{value} of #{suit}"
   end
+end
 
-  def shuffle(deck) do
-    Enum.shuffle(deck)
+@doc """
+  Returns a shuffled list of strings representing a shuffled deck of playing cards.
+"""
+def shuffle(deck) do
+  Enum.shuffle(deck)
+end
+
+@doc """
+  Determines whether a deck contains a given card.
+
+## Examples
+
+      iex> deck = Cards.create_deck
+      iex> Cards.contains?(deck, "Ace of Spades")
+      true
+
+"""
+def contains?(deck, card) do
+  Enum.member?(deck, card)
+end
+
+@doc """
+  Divides a deck into a hand and the remainder of the deck.
+  The `hand_size` argument indicates how many cards should be in the hand.
+
+## Examples
+
+    iex> deck = Cards.create_deck
+    iex> {hand, _} = Cards.deal_hand(deck, 1)
+    iex> hand
+    ["Ace of Spades"]
+
+"""
+def deal_hand(deck, hand_size) do
+  Enum.split(deck, hand_size)
+end
+
+@doc """
+  Saves a binary file to the system, representing a saved deck.
+"""
+def save(deck, filename) do
+  binary = :erlang.term_to_binary(deck)
+  File.write(filename, binary)
+end
+
+def load(filename) do
+  case File.read(filename) do
+    {:ok, binary} -> :erlang.binary_to_term(binary)
+    {:error, _reason} -> "That file does not exist"
   end
+end
 
-  def contains?(deck, card) do
-    Enum.member?(deck, card)
-  end
-
-  def deal_hand(deck, hand_size) do
-    {hand, _} = Enum.split(deck, hand_size)
-
-    hand
-  end
-
-  def save(deck, filename) do
-    binary = :erlang.term_to_binary(deck)
-    File.write(filename, binary)
-  end
-
-  def load(filename) do
-    case File.read(filename) do
-      {:ok, binary} -> :erlang.binary_to_term(binary)
-      {:error, _reason} -> "That file does not exist"
-    end
-  end
-
-  def create_hand(hand_size) do
-    Cards.create_deck
-    |> Cards.shuffle
-    |> Cards.deal_hand(hand_size)
-  end
+def create_hand(hand_size) do
+  Cards.create_deck
+  |> Cards.shuffle
+  |> Cards.deal_hand(hand_size)
+end
 end
