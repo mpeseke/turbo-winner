b9c6e343d68aae861ab931eb882b3e1a1c727d13
Marlon Oliver Peseke <mpeseke@gmail.com>
2023-05-03 17:01:16 -0600

    started identicon project, hashing complete

---
+++ identicon/lib/identicon.ex
@@ -0,0 +1,17 @@
+defmodule Identicon do
+  @moduledoc """
+  Documentation for `Identicon`.
+  """
+
+  def main(input) do
+    input
+    |> hash_input()
+  end
+
+  defp hash_input(input) do
+    hex = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
+  end
+end
